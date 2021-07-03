7065ed2560f628954cc65168b0cfe7f3a31e3c8e
Sumathy-Sridhar <79184088+Sumathy-Sridhar@users.noreply.github.com>
2021-07-03 19:15:40 +0530

    StudentProfile

---
+++ studentProfile/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Studentprofile from './components/Studentprofile';
+
+function App() {
+  return (
+    <div className="App">
+      <Studentprofile/>
+    </div>
+  );
+}
+
+export default App;
